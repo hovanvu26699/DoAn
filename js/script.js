42887f2eae49de0cc64cd7e834c0a425e94d7f6b
hovanvvu26699@gmail.com <hovanvu26699@gmail.com>
2020-08-01 01:23:35 +0700

    vuvu

--- js/script.js
+++ js/script.js
@@ -33,8 +33,30 @@ function MuaSP() {
 }
 
 function MuaHang() {
-    var soLuong = Number(document.getElementById("SoLuongSanPham").value);
+    var soLuong = Number(document.getElementById("SoLuong").value);
     SL += soLuong;
-    document.getElementById("SoLuong").innerHTML = SL;
+    document.getElementById("soLuong").innerHTML = SL;
     alert("Vui lòng đăng kí/đăng nhập để mua hàng");
+}
+//////////////
+function DangKy() {
+    var MatKhau = document.getElementById("txtMK").value;
+    var XN_Matkhau = document.getElementById("txtXN_MK").value;
+    var NS = document.getElementById("txtNS").value;
+    if (MatKhau == XN_Matkhau && GetAge(NS) > 18) {
+        alert("Đăng Nhập Thành Công");
+    } else {
+        alert("Đăng Nhập Thất Bại");
+    }
+}
+
+function GetAge(NS) {
+    var today = new Date();
+    var birth = new Date(NS);
+    var age = today.getFullYear() - birth.getFullYear();
+    var month = today.getMonth() - birth.getMonth();
+    if (month < 0 || (month == 0 && today.getDate() < birth.getDate())) {
+        age--;
+    }
+    return age;
 }
